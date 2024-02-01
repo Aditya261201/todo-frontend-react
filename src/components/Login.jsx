ccbdf817dd69a4fb84353a4f1e8cc9711f2cf6a0
adityaKumar <adityakumarrajput01@gmail.com>
2024-02-01 10:38:17 +0530

    adding toasts and register functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import axios from 'axios';
+import React ,{useState} from 'react'
 import { Link } from 'react-router-dom'
+import toast from "react-hot-toast";
 
 const Login = () => {
+
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState("");
+
+
+    const submitHandler = async(e) => {
+        e.preventDefault();
+        try {
+            const { data } = await axios.post("https://todo-jb1q.onrender.com/api/v1/users/login",
+                {
+                    email, password
+                },
+                {
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    withCredentials: true
+                })
+            toast.success(data.message);
+        } catch (error) {
+            toast.error("Some error occured.");
+            console.log(error);
+        }
+    }
+
     return (
         <div className="login">
             <section>
-                <form action="">
-                    <input type="email" placeholder='Email'/>
-                    <input type="password" name="password" placeholder='password'/>
+                <form onSubmit={submitHandler}>
+                    <input type="email" placeholder='Email' value={email} onChange={(e) => { setemail(e.target.value)}}/>
+                    <input type="password" name="password" placeholder='password' value={password} onChange={(e)=>{setpassword(e.target.value)}}/>
                     <button type='submit'>Login</button>
                     <h4>or</h4>
                     <Link to="/register">Sign Up</Link>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+// import { serverUrl } from '../main.jsx';
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Register = () => {
 
@@ -8,19 +11,35 @@ const Register = () => {
     const [password, setpassword] = useState("");
 
 
-    const submitHandler = (e)=>{
+    const submitHandler = async (e) => {
         e.preventDefault();
-        console.log(name,email,password);
+        try {
+            const { data } = await axios.post("https://todo-jb1q.onrender.com/api/v1/users/new",
+                {
+                    name, email, password
+                },
+                {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                withCredentials: true
+            })
+            toast.success(data.message);
+        } catch (error) {
+            toast.error("Some error occured.");
+            console.log(error);
+        }
     }
+
 
 
     return (
         <div className="login">
             <section>
                 <form onSubmit={submitHandler}>
-                    <input type="text" placeholder='Name' value={name} onChange={(e)=>{setname(e.target.value)}} required/>
-                    <input type="email" placeholder='Email' value={email} onChange={(e) => { setemail(e.target.value) }} required/>
-                    <input type="password" name="password" placeholder='password' value={password} onChange={(e) => { setpassword(e.target.value) }} required/>
+                    <input type="text" placeholder='Name' value={name} onChange={(e) => { setname(e.target.value) }} required />
+                    <input type="email" placeholder='Email' value={email} onChange={(e) => { setemail(e.target.value) }} required />
+                    <input type="password" name="password" placeholder='password' value={password} onChange={(e) => { setpassword(e.target.value) }} required />
                     <button type='submit'>Sign Up</button>
                     <h4>or</h4>
                     <Link to="/login">Log In</Link>
