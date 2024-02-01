e61e3462c000ab18bd43ca548dd75c1b4a845e35
adityaKumar <adityakumarrajput01@gmail.com>
2024-02-02 00:13:54 +0530

    logout functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,27 @@
 import React, { useState,useContext } from 'react'
 import {Link} from "react-router-dom";
 import { Context } from '../main';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const Header = () => {
 
     const { isAuthenticated, setisAuthenticated }=useContext(Context);
 
+    const logoutHandler = ()=>{
+        try {
+            axios.get("https://todo-jb1q.onrender.com/api/v1/users/logout",
+                {
+                    withCredentials: true
+                })
+            setisAuthenticated(false);
+            toast.success("Logged out successfully");
+        } catch (error) {
+            toast.error("Some error occured.");
+            console.log(error);
+            setisAuthenticated(true);
+        }
+    }
 
     return (
         <nav className='header'>
@@ -16,7 +32,7 @@ const Header = () => {
             <Link to={"/"}>Home</Link>
             <Link to={"/profile"}>Profile</Link>
             {
-                isAuthenticated?<button className='btn'>Logout</button>:
+                isAuthenticated?<button className='btn' onClick={logoutHandler}>Logout</button>:
                 <Link to={"/login"}>Login</Link>
             }
 
